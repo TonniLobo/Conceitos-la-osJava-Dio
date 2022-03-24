4c3e110589f78ee9ec97c5d28b82e6c0344c914a
TonniLobo <grohli18@hotmail.com>
2022-03-24 18:10:44 -0300

    Exercicio 3 - arrays - adicionados alguns exemplos de arrays

--- src/br/com/wolfsInn/main/Main.java
+++ src/br/com/wolfsInn/main/Main.java
@@ -5,6 +5,42 @@ public class Main {
         loops();
         interruptores();
     }
+    public static void array(){
+        int[] idades = new int[10];
+        idades[0] = 27;
+        idades[1] = 31;
+        System.out.println("idade 0: "+idades[0]);
+        System.out.println("idade 1: "+idades[1]);
+        System.out.println("idade 2: "+idades[2]);
+
+        int[] i = {10,25};
+        System.out.println("i 0: "+i[0]);
+        System.out.println("i 1: "+i[1]);
+
+        int[] k = new int[]{1,2,3};
+        System.out.println("k 0: "+ k[0]);
+        System.out.println("k 1: "+ k[1]);
+        System.out.println("k 2: "+ k[2]);
+
+        int[] j = new int[2];
+        System.out.println("j 0: "+ j[1]);
+
+        idades = new int[]{12,35,45,22,9,37,51,61};
+
+        for(int idade: idades){
+            System.out.println("Idade: "+idade);
+        }
+
+        for(int a=0; a< idades.length; a++){
+            int idade = idades[a];
+            System.out.println("Idade: "+idade);
+        }
+        for(int idade: idades){
+            if(idade>=18) {
+                System.out.println("Idade: " + idade+ " é maior de 18 anos");
+            }
+        }
+    }
 
     public static void interruptores(){
         for(int i=0; i<=14; i++){
